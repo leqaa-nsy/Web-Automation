270fe7e11fc1e46f74156b590bd54bd1299dcf05
ha45yu-jpg <ha45yu@gmail.com>
2026-05-07 11:48:15 +0300

    Update RegistrationTests.java

--- first_Ayto_script_using_Maven/src/test/java/awesomeqa/RegistrationTests.java
+++ first_Ayto_script_using_Maven/src/test/java/awesomeqa/RegistrationTests.java
@@ -27,4 +27,20 @@ public class RegistrationTests extends BeforAndAfter {
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div[2]/div/div/h1")).getText().contains("Your Account Has Been Created!"));
         Thread.sleep(3000);
     }
+    @Test (priority = 1)
+    public static void invalidRegistration() throws InterruptedException {
+        driver.findElements(By.className("dropdown")).get(0).click();
+        driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/ul/li[1]/a")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("team");
+        driver.findElement(By.id("input-lastname")).sendKeys("1st");
+        driver.findElement(By.id("input-email")).sendKeys("ha45u@gmail.com");
+        driver.findElement(By.id("input-telephone")).sendKeys("01210949358");
+        driver.findElement(By.id("input-password")).sendKeys("teamworkwin");
+        driver.findElement(By.id("input-confirm")).sendKeys("teamworkwin");
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/fieldset[3]/div/div/label[2]/input")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div/input[1]")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div/input[2]")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div[2]/div/div/h1")).getText().contains("Your Account Has Been Created!"));
+        Thread.sleep(3000);
+    }
 }
