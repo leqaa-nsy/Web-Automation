f1cac87e690405c09dec261ab492619018f39e94
ha45yu-jpg <ha45yu@gmail.com>
2026-05-07 10:31:45 +0300

    Update BeforAndAfter.java

--- first_Ayto_script_using_Maven/src/test/java/BeforAndAfter/BeforAndAfter.java
+++ first_Ayto_script_using_Maven/src/test/java/BeforAndAfter/BeforAndAfter.java
@@ -1,1 +1,23 @@
+package BeforAndAfter;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BeforAndAfter
+{
+
+    static WebDriver driver;
+    @BeforeMethod
+    public static void openbrowser(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+    }
+    @AfterMethod
+    public static void closebrowser(){
+        driver.quit();
+    }
+}
