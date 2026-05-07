86d4e2bc963ac64c3d160f9815f6c546844ee285
ha45yu-jpg <ha45yu@gmail.com>
2026-05-07 11:46:43 +0300

    Update RegistrationTests.java

--- first_Ayto_script_using_Maven/src/test/java/awesomeqa/RegistrationTests.java
+++ first_Ayto_script_using_Maven/src/test/java/awesomeqa/RegistrationTests.java
@@ -17,7 +17,7 @@ public class RegistrationTests extends BeforAndAfter {
         driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/ul/li[1]/a")).click();
         driver.findElement(By.id("input-firstname")).sendKeys("team");
         driver.findElement(By.id("input-lastname")).sendKeys("1st");
-        driver.findElement(By.id("input-email")).sendKeys("ha45u@gmail.com");
+        driver.findElement(By.id("input-email")).sendKeys("ha45y@gmail.com");
         driver.findElement(By.id("input-telephone")).sendKeys("01210949358");
         driver.findElement(By.id("input-password")).sendKeys("teamworkwin");
         driver.findElement(By.id("input-confirm")).sendKeys("teamworkwin");
